d617c48c663d3777ddd4b025ad517cdc1f95338c
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-20 12:32:16 +0100

    Init 2

---
+++ assets/dist/js/app.js
@@ -0,0 +1,27 @@
+var FamiljenHbg;
+
+FamiljenHbg = FamiljenHbg || {};
+FamiljenHbg.ExampleNamespace = FamiljenHbg.Liquid || {};
+
+FamiljenHbg.ExampleNamespace.ExampleClass = (function ($) {
+
+	var classVariable = false;
+
+    /**
+     * Constructor
+     * Should be named as the class itself
+     */
+	function ExampleClass() {
+
+    }
+
+    /**
+     * Method
+     */
+    ExampleClass.prototype.exampleMethod = function () {
+
+    }
+
+	return new ExampleClass();
+
+})(jQuery);
