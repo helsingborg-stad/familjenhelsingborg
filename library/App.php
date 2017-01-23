d561647d6bd78bd7a05ced23864c51d945e9267f
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-23 16:29:44 +0100

    Get term label background and text color

--- library/App.php
+++ library/App.php
@@ -5,6 +5,7 @@ class App
 {
     public function __construct()
     {
+        new \FamiljenHbg\Theme\General();
         new \FamiljenHbg\Theme\Enqueue();
         new \FamiljenHbg\Theme\Authors();
     }
