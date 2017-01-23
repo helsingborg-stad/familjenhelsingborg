2c1aba1818b4ce9e5c6f36fb3dde670dba17b475
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-23 16:38:04 +0100

    Remove terms filter

--- library/App.php
+++ library/App.php
@@ -5,7 +5,6 @@ class App
 {
     public function __construct()
     {
-        new \FamiljenHbg\Theme\General();
         new \FamiljenHbg\Theme\Enqueue();
         new \FamiljenHbg\Theme\Authors();
     }
