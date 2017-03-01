26c6700cf247dd2b49fa9f7fc6f8ed80b0b248d6
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-01 08:44:01 +0100

    Filter authors without full name

--- library/Controller/Authors.php
+++ library/Controller/Authors.php
@@ -6,8 +6,19 @@ class Authors extends \Municipio\Controller\BaseController
 {
     public function init()
     {
-        $this->data['authors'] = get_users(array(
+        $this->data['authors'] = $this->getAuthors();
+    }
+
+    public function getAuthors()
+    {
+        $authors = get_users(array(
             'who' => 'authors'
         ));
+
+        $authors = array_filter($authors, function ($author) {
+            return municipio_get_author_full_name($author->ID);
+        });
+
+        return $authors;
     }
 }
