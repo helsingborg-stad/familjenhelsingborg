c9e31c4391e94fe276726854db1199bc2e099607
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-01 08:44:16 +0100

    Show placeholder profile image for authors without uploaded profile image

--- views/authors.blade.php
+++ views/authors.blade.php
@@ -13,11 +13,18 @@
             <?php $i++; if ($i > 5) $i = 1; ?>
             <div class="grid-md-3">
                 <a href="{{ get_author_posts_url($author->ID) }}" class="box box-filled box-filled-{{ $i }} box-author">
-                    <img src="{{ get_the_author_meta('user_profile_picture', $author->ID) }}" alt="{{ get_the_author_meta('nicename', $author->ID) }}" class="box-image">
+                    @if (get_the_author_meta('user_profile_picture', $author->ID))
+                    <img src="{{ get_the_author_meta('user_profile_picture', $author->ID) }}" alt="{{ municipio_get_author_full_name($author->ID) }}" class="box-image">
+                    @else
+                    <figure class="box-image image-placeholder" style="width: 150px;height: 150px;padding-bottom: 0;color: #000;"></figure>
+                    @endif
 
                     <div class="box-content">
                         <div class="author-name">{{ municipio_get_author_full_name($author->ID) }}</div>
+
+                        @if (get_the_author_meta('description', $author->ID))
                         <div class="author-description">{{ get_the_author_meta('description', $author->ID) }}</div>
+                        @endif
                     </div>
                 </a>
             </div>
