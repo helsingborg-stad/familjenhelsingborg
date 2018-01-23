cfa23bb393338ecef817d70485233a5e03d9c70e
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-01-23 10:04:47 +0100

    Added area view data

---
+++ library/Controller/SingleArea.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace FamiljenHbg\Controller;
+
+class SingleArea extends \Municipio\Controller\BaseController
+{
+    public function init()
+    {
+        $this->data['facts'] = $this->getFacts();
+        $this->data['links'] = $this->getLinkList();
+        $this->data['images'] = $this->getRegionImages();
+        $this->data['language'] = array(
+            'readmore' => __("Read more about this location:", 'familjen-hbg')
+        );
+    }
+
+    public function getFacts()
+    {
+
+        //Get basic data structure
+        $facts = array(
+            'general' => get_field('area_general_facts'),
+            'metadata' => get_field('area_metadata')
+        );
+
+        //Only get first item of array
+        if (is_array($facts['metadata']) && !empty($facts['metadata'])) {
+            foreach ($facts['metadata'] as &$metaItem) {
+                $metaItem['area_heading'] = array_pop($metaItem['area_heading']);
+            }
+        }
+
+        //Return structured data
+        return $facts;
+    }
+
+    public function getLinkList()
+    {
+        //Get list of links
+        $links = get_field('area_external');
+
+        //Check if is valid data
+        if (is_array($links) && !empty($links)) {
+            return $links;
+        }
+
+        return null;
+    }
+
+    public function getRegionImages($response = array())
+    {
+
+        //Get images
+        $images = get_field('location_region_images');
+
+        //Format in loop
+        if (isset($images) && is_array($images) && !empty($images)) {
+            foreach ($images as $image) {
+
+                //Base data
+                $item = array(
+                    'caption' => $image['caption'],
+                    'large' => $image['sizes']['large']
+                );
+
+                //Get thumbnail img src
+                if ($image = wp_get_attachment_image_src($image['id'], array(200, 150))) {
+                    $item['thumbnail'] = isset($image[0]) && !empty($image[0]) ? $image[0] : null;
+                }
+
+                $response[] = $item;
+            }
+        }
+
+        return $response;
+    }
+}
---
+++ views/single-area.blade.php
@@ -0,0 +1,112 @@
+@extends('templates.master')
+
+@section('content')
+
+<div class="container main-container">
+
+    @include('partials.breadcrumbs')
+
+    <div class="blehe grid {{ implode(' ', apply_filters('Municipio/Page/MainGrid/Classes', wp_get_post_parent_id(get_the_id()) != 0 ? array('no-margin-top') : array())) }}">
+        @include('partials.sidebar-left')
+
+        <div class="grid-md-8 grid-lg-6 grid-print-12 grid-print-12" id="readspeaker-read">
+
+            @if (is_active_sidebar('content-area-top'))
+                <div class="grid sidebar-content-area sidebar-content-area-top">
+                    <?php dynamic_sidebar('content-area-top'); ?>
+                </div>
+            @endif
+
+            @while(have_posts())
+                {!! the_post() !!}
+
+                @include('partials.article')
+
+                <!-- External links -->
+                @if(isset($links) && is_array($links) && !empty($links))
+                    <div class="grid">
+                        <div class="grid-xs-12">
+                            <h3 class="read-more-about-this">{{$language['readmore']}}</h3>
+                            <ul class="area-link-list">
+                                @foreach($links as $linkItem)
+                                    <li class="{{sanitize_title($linkItem['link_label'])}} inline-block">
+                                        <a class="btn btn-md btn-primary" href="{{$linkItem['link_url']}}">
+                                            <span class="link-item link-item-outbound">{{$linkItem['link_label']}}</span>
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                @endif
+
+            @endwhile
+
+            @if (is_active_sidebar('content-area'))
+                <div class="grid sidebar-content-area sidebar-content-area-bottom">
+                    <?php dynamic_sidebar('content-area'); ?>
+                </div>
+            @endif
+
+            <div class="hidden-xs hidden-sm hidden-md hidden-print">
+                @include('partials.page-footer')
+            </div>
+        </div>
+
+        <aside class="grid-lg-3 grid-md-12 sidebar-right-sidebar hidden-xs hidden-sm hidden-md">
+            <div class="grid">
+
+                <!-- Region images -->
+                @if(isset($images) && is_array($images) && !empty($images))
+                    <ul class="image-gallery grid grid-gallery">
+                        @foreach($images as $image)
+                            <li class="grid-md-6">
+                                <a class="box lightbox-trigger" href="{{$image['large']}}">
+                                    <img alt="{{$image['caption']}}" src="{{$image['thumbnail']}}"/>
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                @endif
+
+                <!-- Facts -->
+                @if(isset($facts))
+
+                    <!-- General -->
+                    @if(isset($facts['general']) && !empty($facts['general']))
+                        {{ $facts['general'] }}
+                    @endif
+
+                    <!-- Metadata -->
+                    @if(isset($facts['metadata']) && is_array($facts['metadata']) && !empty($facts['metadata']))
+                        <ul>
+                            @foreach($facts['metadata'] as $metaItem)
+                                <li class="area-metadata-list area-metadata-list-{{ $metaItem['area_heading']['value'] }} c-area-metadata-list t-area-metadata-list">
+                                    <!-- Heading -->
+                                    @if(isset($metaItem['area_heading']) && !empty($metaItem['area_heading']) && is_array($metaItem['area_heading']))
+                                        <span class="area-heading c-area-heading t-area-heading">{{ $metaItem['area_heading']['label'] }}</span>
+                                    @endif
+
+                                    <!-- Area information -->
+                                    @if(isset($metaItem['area_information']) && !empty($metaItem['area_information']))
+                                        <span class="area-information c-area-information t-area-information">{{ $metaItem['area_information'] }}</span>
+                                    @endif
+                                </li>
+                            @endforeach
+                        </ul>
+                    @endif
+                @endif
+
+            </div>
+        </aside>
+
+    </div>
+
+    <div class="grid hidden-lg hidden-xl">
+        <div class="grid-sm-12">
+            @include('partials.page-footer')
+        </div>
+    </div>
+</div>
+
+@stop
