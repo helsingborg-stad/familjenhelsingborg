67de1ea41cc8eaa03e25a59dbf4d1a135d9d1a28
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-01-29 15:00:20 +0100

    Submit feedback in button apperance

--- views/partials/page-footer.blade.php
+++ views/partials/page-footer.blade.php
@@ -3,8 +3,11 @@
 <footer class="page-footer">
     @if (get_field('post_show_share', get_the_id()) !== false && get_field('page_show_share', 'option') !== false)
     <div class="grid">
-        <div class="grid-xs-12">
-                @include('partials.social-share')
+        <div class="grid-xs-8">
+            @include('partials.social-share')
+        </div>
+        <div class="grid-xs-4 text-right">
+            <a href="#customer-feedback" class="btn btn-lg btn-outline btn-primary"><?php _e("Submit feedback on this page", 'familjen-hbg'); ?></a>
         </div>
     </div>
     @endif
