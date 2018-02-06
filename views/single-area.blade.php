0aa526e9ae0a983ac8016ac5a8df4bb24c14b26c
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-02-06 08:30:11 +0100

    Removes old test class

--- views/single-area.blade.php
+++ views/single-area.blade.php
@@ -6,7 +6,7 @@
 
     @include('partials.breadcrumbs')
 
-    <div class="blehe grid {{ implode(' ', apply_filters('Municipio/Page/MainGrid/Classes', wp_get_post_parent_id(get_the_id()) != 0 ? array('no-margin-top') : array())) }}">
+    <div class="grid {{ implode(' ', apply_filters('Municipio/Page/MainGrid/Classes', wp_get_post_parent_id(get_the_id()) != 0 ? array('no-margin-top') : array())) }}">
         @include('partials.sidebar-left')
 
         <div class="grid-md-8 grid-lg-6 grid-print-12 grid-print-12" id="readspeaker-read">
