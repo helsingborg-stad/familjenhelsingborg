0cb11a78967022bc25f5e89ac9c1c4a026ebec93
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-02-26 12:55:41 +0100

    fix spacing, bold label & formatting

--- views/single-area.blade.php
+++ views/single-area.blade.php
@@ -4,8 +4,7 @@
 <div class="u-block has-elements u-no-padding-top">
     <div class="container u-elements">
         @include('partials.breadcrumbs')
-
-        <div class="grid {{ implode(' ', apply_filters('Municipio/Page/MainGrid/Classes', wp_get_post_parent_id(get_the_id()) != 0 ? array('no-margin-top') : array())) }}">
+        <div class="grid no-margin-bottom {{ implode(' ', apply_filters('Municipio/Page/MainGrid/Classes', wp_get_post_parent_id(get_the_id()) != 0 ? array('no-margin-top') : array())) }}">
 
             <div class="grid-lg-7 grid-sm-12 grid-xs-12 grid-print-12" id="readspeaker-read">
                 @if (is_active_sidebar('content-area-top'))
@@ -56,71 +55,77 @@
                     @endif
             </aside>
         </div>
-        <div class="grid">
 
-            <!-- Map -->
-                @if(isset($location) && is_array($location) && isset($location['address']))
-                <div class="box grid-lg-4 no-margin">
-                    <div class="box-content no-padding">
-                    <div class="area-map c-area-map t-area-map ratio-1-1">
-                        <div id="areaMap" class="ratio-1-1" style="position: absolute; top: 0; right: 0; bottom: 0; left: 0"></div>
-                        <script>
-                          function areaInitMap() {
-                            var map = new google.maps.Map(document.getElementById('areaMap'), {
-                              zoom: 11,
-                              center: {lat: {{$location['lat']}}, lng: {{$location['lng']}}},
-                              disableDefaultUI: true
-                            });
+        <div class="grid no-margin-top">
+            <div class="grid-xs-12">
+                <div class="box">
+                    <div class="grid no-margin">
+                        <!-- Map -->
+                        @if(isset($location) && is_array($location) && isset($location['address']))
+                        <div class="grid-lg-4 no-margin no-padding">
+                            <div class="box-content no-padding">
+                            <div class="area-map c-area-map t-area-map ratio-1-1">
+                                <div id="areaMap" class="ratio-1-1" style="position: absolute; top: 0; right: 0; bottom: 0; left: 0"></div>
+                                <script>
+                                  function areaInitMap() {
+                                    var map = new google.maps.Map(document.getElementById('areaMap'), {
+                                      zoom: 11,
+                                      center: {lat: {{$location['lat']}}, lng: {{$location['lng']}}},
+                                      disableDefaultUI: true
+                                    });
 
-                            var marker = new google.maps.Marker({
-                              position: {lat: {{$location['lat']}}, lng: {{$location['lng']}}},
-                              map: map,
-                              title: '!'
-                            });
+                                    var marker = new google.maps.Marker({
+                                      position: {lat: {{$location['lat']}}, lng: {{$location['lng']}}},
+                                      map: map,
+                                      title: '!'
+                                    });
 
-                          }
-                        </script>
-                        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBcTrRdDFsoCu3bNbfBMU5Me1-9iqChOM8&callback=areaInitMap"></script>
-                    </div>
+                                  }
+                                </script>
+                                <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBcTrRdDFsoCu3bNbfBMU5Me1-9iqChOM8&callback=areaInitMap"></script>
+                            </div>
+                            </div>
+                        </div>
+                        @endif
+
+                        <!-- Metadata -->
+                        @if(isset($facts['metadata']) && is_array($facts['metadata']) && !empty($facts['metadata']))
+                            <div class="grid-lg-4 no-margin">
+                                <div class="box-content">
+                                    <h4 class="box-title">Fakta</h4>
+                                    <ul class="unordered-list">
+                                        @foreach($facts['metadata'] as $metaItem)
+                                            <li class="area-metadata-list area-metadata-list-{{ $metaItem['area_heading']['value'] }} c-area-metadata-list t-area-metadata-list">
+                                                <!-- Heading -->
+                                                @if(isset($metaItem['area_heading']) && !empty($metaItem['area_heading']) && is_array($metaItem['area_heading']))
+                                                    <strong class="area-heading c-area-heading t-area-heading">{{ $metaItem['area_heading']['label'] }}: </strong>
+                                                @endif
+
+                                                <!-- Area information -->
+                                                @if(isset($metaItem['area_information']) && !empty($metaItem['area_information']))
+                                                    <span class="area-information c-area-information t-area-information">{{ $metaItem['area_information'] }}</span>
+                                                @endif
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
+                        @endif
+
+                        <!-- General -->
+                        @if(isset($facts['general']) && !empty($facts['general']))
+                            <div class="grid-lg-4 no-margin no-padding">
+                                <div class="box-content">
+                                    <h4 class="box-title">Om <?php the_title(); ?></h4>
+                                    <p>
+                                        {{ $facts['general'] }}
+                                    </p>
+                                </div>
+                            </div>
+                        @endif
                     </div>
                 </div>
-                @endif
-
-                <!-- Metadata -->
-                @if(isset($facts['metadata']) && is_array($facts['metadata']) && !empty($facts['metadata']))
-                    <div class="box grid-lg-4 no-margin">
-                        <div class="box-content">
-                            <h4 class="box-title">Fakta</h4>
-                            <ul class="unordered-list">
-                                @foreach($facts['metadata'] as $metaItem)
-                                    <li class="area-metadata-list area-metadata-list-{{ $metaItem['area_heading']['value'] }} c-area-metadata-list t-area-metadata-list">
-                                        <!-- Heading -->
-                                        @if(isset($metaItem['area_heading']) && !empty($metaItem['area_heading']) && is_array($metaItem['area_heading']))
-                                            <span class="area-heading c-area-heading t-area-heading">{{ $metaItem['area_heading']['label'] }}</span>
-                                        @endif
-
-                                        <!-- Area information -->
-                                        @if(isset($metaItem['area_information']) && !empty($metaItem['area_information']))
-                                            <span class="area-information c-area-information t-area-information">{{ $metaItem['area_information'] }}</span>
-                                        @endif
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                @endif
-
-                <!-- General -->
-                @if(isset($facts['general']) && !empty($facts['general']))
-                    <div class="box grid-lg-4 no-margin">
-                        <div class="box-content">
-                            <h4 class="box-title">Om <?php the_title(); ?></h4>
-                            <p>
-                                {{ $facts['general'] }}
-                            </p>
-                        </div>
-                    </div>
-                @endif
+            </div>
         </div>
     </div>
 </div>
