0c630404ab12b12cde64737bdbe47f35625bf79d
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-03-07 16:32:01 +0100

    Filter archive filter position options

--- library/App.php
+++ library/App.php
@@ -5,6 +5,7 @@ class App
 {
     public function __construct()
     {
+        new \FamiljenHbg\Admin\ArchiveOptions();
         new \FamiljenHbg\Theme\CustomizerFeatures();
         new \FamiljenHbg\Theme\Enqueue();
         new \FamiljenHbg\Theme\Color();
